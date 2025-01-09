a837a6b06b6577b60e9d27c04fd805368a43b72a
Easy Agent <support@ezxs.events>
2025-01-09 02:01:42 -0400

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://jvsc99.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
+        path: 'https://github.com/ezxslife/preloaderJSLottie/blob/main/Artboard%201.json',
     });
 }
 
